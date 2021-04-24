6b076da31c17acab2baeafa8e758a88c08f74749
los0731 <los0731@gmail.com>
2021-04-24 21:42:20 +0900

    bot update

--- venv/telegram_bot.py
+++ venv/telegram_bot.py
@@ -1,5 +1,7 @@
 from functions import *
 import telegram
+import schedule
+import time
 
 my_token = "1702806418:AAFNyJ2J_seTRLid2nsUc40xeLf3l2c1JvI"
 # my_chat_id = "1769603089" #alfred 봇 개인챗
@@ -7,7 +9,7 @@ my_chat_id = "@aaaalfred" #aaaalfred 채널
 # today = datetime.today().strftime("%Y-%m-%d")
 yesterday = datetime.today() - timedelta(1)
 yesterday = yesterday.strftime("%Y-%m-%d") 
-
+week = datetime.today().weekday()
 bot = telegram.Bot(token = my_token)
 updates = bot.getUpdates()
 
@@ -24,4 +26,16 @@ def sendIndustries():
     # print(content)
     bot.sendMessage(chat_id = my_chat_id, text=content)
 
-sendIndustries()
+def job():
+    if week < 5:
+        sendIndustries()
+    else:
+        pass
+
+# 매일 10:30 에 실행
+# schedule.every().day.at("07:30").do(job)
+schedule.every(10).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
